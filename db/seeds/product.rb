c21a27535967bf01e68c5aad8170640445346ce2
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2021-09-11 00:27:29 +0900

    product

--- db/seeds/product.rb
+++ db/seeds/product.rb
@@ -1,5 +1,5 @@
 Product.create(product_name: '銀イオン詰替え用（6ppm）2L', price: 3000, description: "3種類の保湿剤を配合し、手に潤いを与えるだけでなく、さらさら・すべすべ感も与えます。有効成分として「ベンザルコニウム塩化物」使用。広範囲の微生物に対して短時間で効力を発揮します。手でつぶせる容器です。", stock: 0, model_number: "AG010", product_type: "消毒液")
-product = Product.find(1)
+product = Product.find(5)
 product.images.attach([io: File.open('app/assets/images/消毒液1_1.png'), filename: '消毒液1_1.png'], [io: File.open('app/assets/images/消毒液1_2.png'), filename: '消毒液1_2.png'])
 Product.create(product_name: '銀イオン詰替え用（20ppm）2L', price: 5000, description: "1．ポンプはゆっくり押して、適量（500円玉程度）をお取りください。 2． およそ15秒間、両手にまんべんなく塗り広げてください。 3．アルコールが完全に揮発するまで両手をすり合わせてください。", stock: 1, model_number: "AG011", product_type: "消毒液")
 product = Product.find(2)
